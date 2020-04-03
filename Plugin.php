5dfad21b817467a04f844e698a9facdad2a11dcd
yfktn <friskantoni@gmail.com>
2020-04-03 17:02:33 +0700

    Version 0.0.5

--- Plugin.php
+++ Plugin.php
@@ -6,6 +6,7 @@ use Twig\CacheExtension\CacheStrategy\LifetimeCacheStrategy;
 use Twig\CacheExtension\Extension as CacheExtension;
 use Twig\CacheExtension\CacheStrategy\IndexedChainingCacheStrategy;
 use Twig\CacheExtension\CacheStrategy\GenerationalCacheStrategy;
+use Twig\CacheExtension\CacheStrategy\BlackholeCacheStrategy;
 use Madewithlove\IlluminatePsrCacheBridge\Laravel\CacheItemPool;
 use Illuminate\Contracts\Cache\Repository;
 use Yfktn\TwigCachePlugin\Classes\ModelKeyGenerator;
@@ -17,18 +18,23 @@ class Plugin extends PluginBase
 
     public function boot()
     {
-        \Event::Listen('cms.page.beforeDisplay', function (Controller $controller, $url, $page) {
-
-            $repository = \App::make(Repository::class);
-            $cacheProvider = new PsrCacheAdapter(new CacheItemPool($repository));
-            $cacheStrategy = new IndexedChainingCacheStrategy([
-                'time' => new LifetimeCacheStrategy($cacheProvider),
-                'model' => new GenerationalCacheStrategy(
-                    $cacheProvider, 
-                    new ModelKeyGenerator(), 
-                    7200 /* 0 = infinite lifetime */)
-            ]);
-            $cacheExtension = new CacheExtension($cacheStrategy);
+        Event::Listen('cms.page.beforeDisplay', function (Controller $controller, $url, $page) {
+            if( config('Yfktn.TwigCachePlugin::blackholeCacheStrategyMode', false) ) {
+                // trace_log('blackhole not activated');
+                $repository = \App::make(Repository::class);
+                $cacheProvider = new PsrCacheAdapter(new CacheItemPool($repository));
+                $cacheStrategy = new IndexedChainingCacheStrategy([
+                    'time' => new LifetimeCacheStrategy($cacheProvider),
+                    'model' => new GenerationalCacheStrategy(
+                        $cacheProvider, 
+                        new ModelKeyGenerator(), 
+                        config('Yfktn.TwigCachePlugin::modelCacheStrategyLifetime', 7200) /* 0 = infinite lifetime */)
+                ]);
+                $cacheExtension = new CacheExtension($cacheStrategy);
+            } else {
+                // trace_log('blackhole activated');
+                $cacheExtension = new CacheExtension(new BlackholeCacheStrategy());
+            }
 
             $twig = $controller->getTwig();
             if (!$twig->hasExtension('Twig\CacheExtension\Extension')) {
---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * File configuration for TwigCachePlugin
+ */
+ return [
+     /**
+      * When this value is true, then we running Cache Strategy which doesn't cache at all!
+      * this strategy prevent previously cached version from being rendered. Very useful when in development mode.
+      */
+     'blackholeCacheStrategyMode' => 'true',
+     'modelCacheStrategyLifetime' => 7200
+ ];
